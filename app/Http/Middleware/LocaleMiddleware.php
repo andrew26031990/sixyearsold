80da976bdc6342291c25419f1a54f305a9e47c22
Andrew <mandrew3601@gmail.com>
2020-11-16 10:07:40 +0500

    language issue fixed

---
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class LocaleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::user()){
+            app()->setLocale(Auth::user()->locale);
+        }elseif($locale = Session::has('locale')){
+            app()->setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
